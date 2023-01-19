bec8875db6ea7de0fb0786bbdb2af89fea44836b
Milos Jancovic <mjancovic@qualityunit.com>
2023-01-19 17:14:18 +0100

    build: 5.0.2

--- dist/autosize.js
+++ dist/autosize.js
@@ -2,7 +2,7 @@
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
 	(global = global || self, global.autosize = factory());
-}(this, (function () {
+})(this, (function () {
 	var map = typeof Map === "function" ? new Map() : function () {
 	  var keys = [];
 	  var values = [];
@@ -21,7 +21,6 @@
 	    },
 	    "delete": function _delete(key) {
 	      var index = keys.indexOf(key);
-
 	      if (index > -1) {
 	        keys.splice(index, 1);
 	        values.splice(index, 1);
@@ -29,13 +28,11 @@
 	    }
 	  };
 	}();
-
 	var createEvent = function createEvent(name) {
 	  return new Event(name, {
 	    bubbles: true
 	  });
 	};
-
 	try {
 	  new Event('test');
 	} catch (e) {
@@ -46,95 +43,100 @@
 	    return evt;
 	  };
 	}
-
 	function assign(ta) {
 	  if (!ta || !ta.nodeName || ta.nodeName !== 'TEXTAREA' || map.has(ta)) return;
 	  var heightOffset = null;
 	  var clientWidth = null;
 	  var cachedHeight = null;
-
+	  var textLength = null;
+	  var resizeTimer = null;
 	  function init() {
 	    var style = window.getComputedStyle(ta, null);
-
 	    if (style.resize === 'vertical') {
 	      ta.style.resize = 'none';
 	    } else if (style.resize === 'both') {
 	      ta.style.resize = 'horizontal';
 	    }
-
 	    if (style.boxSizing === 'content-box') {
 	      heightOffset = -(parseFloat(style.paddingTop) + parseFloat(style.paddingBottom));
 	    } else {
 	      heightOffset = parseFloat(style.borderTopWidth) + parseFloat(style.borderBottomWidth);
-	    } // Fix when a textarea is not on document body and heightOffset is Not a Number
-
-
+	    }
+	    // Fix when a textarea is not on document body and heightOffset is Not a Number
 	    if (isNaN(heightOffset)) {
 	      heightOffset = 0;
 	    }
-
 	    update();
 	  }
-
 	  function changeOverflow(value) {
 	    {
 	      // Chrome/Safari-specific fix:
 	      // When the textarea y-overflow is hidden, Chrome/Safari do not reflow the text to account for the space
 	      // made available by removing the scrollbar. The following forces the necessary text reflow.
 	      var width = ta.style.width;
-	      ta.style.width = '0px'; // Force reflow:
+	      ta.style.width = '0px';
 	      /* jshint ignore:end */
-
 	      ta.style.width = width;
 	    }
 	    ta.style.overflowY = value;
 	  }
-
-	  function bookmarkOverflows(el) {
+	  function cacheScrollTops(el) {
 	    var arr = [];
-
 	    while (el && el.parentNode && el.parentNode instanceof Element) {
 	      if (el.parentNode.scrollTop) {
-	        el.parentNode.style.scrollBehavior = 'auto';
 	        arr.push([el.parentNode, el.parentNode.scrollTop]);
 	      }
-
 	      el = el.parentNode;
 	    }
-
 	    return function () {
 	      return arr.forEach(function (_ref) {
 	        var node = _ref[0],
-	            scrollTop = _ref[1];
+	          scrollTop = _ref[1];
+	        node.style.scrollBehavior = 'auto';
 	        node.scrollTop = scrollTop;
 	        node.style.scrollBehavior = null;
 	      });
 	    };
 	  }
-
 	  function resize() {
 	    if (ta.scrollHeight === 0) {
 	      // If the scrollHeight is 0, then the element probably has display:none or is detached from the DOM.
 	      return;
-	    } // remove smooth scroll & prevent scroll-position jumping by restoring original scroll position
+	    }
 
+	    // ensure the scrollTop values of parent elements are not modified as a consequence of calculating the textarea height
+	    var restoreScrollTops = cacheScrollTops(ta);
+	    if (ta.style.height !== getNewHeight()) {
+	      refreshSize();
+	    } else if (textLength > ta.textLength) {
+	      if (resizeTimer != null) {
+	        clearTimeout(resizeTimer);
+	      }
+	      resizeTimer = setTimeout(refreshSize, 200);
+	    }
+	    textLength = ta.textLength;
 
-	    var restoreOverflows = bookmarkOverflows(ta);
-	    ta.style.height = '';
-	    ta.style.height = ta.scrollHeight + heightOffset + 'px'; // used to check if an update is actually necessary on window.resize
-
+	    // used to check if an update is actually necessary on window.resize
 	    clientWidth = ta.clientWidth;
-	    restoreOverflows();
+	    restoreScrollTops();
 	  }
-
+	  function refreshSize() {
+	    ta.style.height = '';
+	    ta.style.height = getNewHeight();
+	  }
+	  function getNewHeight() {
+	    return ta.scrollHeight + heightOffset + 'px';
+	  }
 	  function update() {
 	    resize();
 	    var styleHeight = Math.round(parseFloat(ta.style.height));
-	    var computed = window.getComputedStyle(ta, null); // Using offsetHeight as a replacement for computed.height in IE, because IE does not account use of border-box
+	    var computed = window.getComputedStyle(ta, null);
 
-	    var actualHeight = computed.boxSizing === 'content-box' ? Math.round(parseFloat(computed.height)) : ta.offsetHeight; // The actual height not matching the style height (set via the resize method) indicates that
+	    // Using offsetHeight as a replacement for computed.height in IE, because IE does not account use of border-box
+	    var actualHeight = computed.boxSizing === 'content-box' ? Math.round(parseFloat(computed.height)) : ta.offsetHeight;
+
+	    // The actual height not matching the style height (set via the resize method) indicates that
 	    // the max-height has been exceeded, in which case the overflow should be allowed.
-
 	    if (actualHeight < styleHeight) {
 	      if (computed.overflowY === 'hidden') {
 	        changeOverflow('scroll');
@@ -149,25 +151,22 @@
 	        actualHeight = computed.boxSizing === 'content-box' ? Math.round(parseFloat(window.getComputedStyle(ta, null).height)) : ta.offsetHeight;
 	      }
 	    }
-
 	    if (cachedHeight !== actualHeight) {
 	      cachedHeight = actualHeight;
 	      var evt = createEvent('autosize:resized');
-
 	      try {
 	        ta.dispatchEvent(evt);
-	      } catch (err) {// Firefox will throw an error on dispatchEvent for a detached element
+	      } catch (err) {
+	        // Firefox will throw an error on dispatchEvent for a detached element
 	        // https://bugzilla.mozilla.org/show_bug.cgi?id=889376
 	      }
 	    }
 	  }
-
 	  var pageResize = function pageResize() {
 	    if (ta.clientWidth !== clientWidth) {
 	      update();
 	    }
 	  };
-
 	  var destroy = function (style) {
 	    window.removeEventListener('resize', pageResize, false);
 	    ta.removeEventListener('input', update, false);
@@ -185,15 +184,14 @@
 	    overflowX: ta.style.overflowX,
 	    wordWrap: ta.style.wordWrap
 	  });
+	  ta.addEventListener('autosize:destroy', destroy, false);
 
-	  ta.addEventListener('autosize:destroy', destroy, false); // IE9 does not fire onpropertychange or oninput for deletions,
+	  // IE9 does not fire onpropertychange or oninput for deletions,
 	  // so binding to onkeyup to catch most of those events.
 	  // There is no way that I know of to detect something like 'cut' in IE9.
-
 	  if ('onpropertychange' in ta && 'oninput' in ta) {
 	    ta.addEventListener('keyup', update, false);
 	  }
-
 	  window.addEventListener('resize', pageResize, false);
 	  ta.addEventListener('input', update, false);
 	  ta.addEventListener('autosize:update', update, false);
@@ -205,34 +203,28 @@
 	  });
 	  init();
 	}
-
 	function destroy(ta) {
 	  var methods = map.get(ta);
-
 	  if (methods) {
 	    methods.destroy();
 	  }
 	}
-
 	function update(ta) {
 	  var methods = map.get(ta);
-
 	  if (methods) {
 	    methods.update();
 	  }
 	}
+	var autosize = null;
 
-	var autosize = null; // Do nothing in Node.js environment and IE8 (or lower)
-
+	// Do nothing in Node.js environment and IE8 (or lower)
 	if (typeof window === 'undefined' || typeof window.getComputedStyle !== 'function') {
 	  autosize = function autosize(el) {
 	    return el;
 	  };
-
 	  autosize.destroy = function (el) {
 	    return el;
 	  };
-
 	  autosize.update = function (el) {
 	    return el;
 	  };
@@ -243,29 +235,23 @@
 	        return assign(x);
 	      });
 	    }
-
 	    return el;
 	  };
-
 	  autosize.destroy = function (el) {
 	    if (el) {
 	      Array.prototype.forEach.call(el.length ? el : [el], destroy);
 	    }
-
 	    return el;
 	  };
-
 	  autosize.update = function (el) {
 	    if (el) {
 	      Array.prototype.forEach.call(el.length ? el : [el], update);
 	    }
-
 	    return el;
 	  };
 	}
-
 	var autosize$1 = autosize;
 
 	return autosize$1;
 
-})));
+}));
